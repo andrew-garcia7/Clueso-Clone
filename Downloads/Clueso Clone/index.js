9d4d2d4431b7f50752032ad48bb78be7e7c39f00
andrew-garcia7 <opandrew00@gmail.com>
2025-12-26 17:51:31 +0530

    fix: use dynamic PORT for Render deployment

--- Downloads/Clueso Clone/index.js
+++ Downloads/Clueso Clone/index.js
@@ -15,7 +15,8 @@ const { generateInsights } = require('./services/aiService');
 
 // Create Express app
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 
 // Middleware to parse JSON requests
 app.use(express.json());
